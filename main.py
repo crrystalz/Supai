367abc2571edc5c98d9407404a747f927aba33b9
crystalz <lichupatnaik@gmail.com>
2022-04-30 11:20:44 -0700

    Gets number of coins in buy orders for each item in bazaar
    tutorial used: https://www.youtube.com/watch?v=8fNn3be5z9Y&list=PL8Uh__5X0ZE5F66qX9yiYMHkMhih-l_UU&index=3

---
+++ main.py
@@ -0,0 +1,49 @@
+import requests
+import json
+from pprint import pprint
+
+
+# Returns GET request response from url
+def get_info(call):
+    r = requests.get(call)
+    return r.json()
+
+
+# Returns all Bazaar data
+def get_bazaar_data():
+    return get_info("https://api.hypixel.net/skyblock/bazaar")
+
+
+# Returns total coin counts in buy orders on the bazaar
+def get_bazaar_buy_order_value(bazaar_data):
+    sum_coins = 0
+    price_increase_threshold = 1.2
+
+    # For every product
+    for item_name, item_data in bazaar_data.get("products", {}).items():
+
+        item_sum_coins = 0
+
+        # For every buy order
+        for idx, buy_order in enumerate(item_data.get("buy_summary", [])):
+
+            # If it's the best price
+            if idx == 0:
+                item_expected_value = buy_order.get("pricePerUnit", 0)
+                item_sum_coins += buy_order.get("amount", 0) * buy_order.get("pricePerUnit", 0)
+
+            # If it's not the best price, check for reasonable price
+            else:
+                if buy_order.get("pricePerUnit", 0) < (item_expected_value * price_increase_threshold):
+                    item_sum_coins += buy_order.get("amount", 0) * buy_order.get("pricePerUnit", 0)
+
+        print(f"{item_name} | {round(item_sum_coins)}")
+        sum_coins += item_sum_coins
+
+    return sum_coins
+
+# API_KEY.json is not in GitHub repo
+API_FILE = open("API_KEY.json", "r")
+API_KEY = json.loads(API_FILE.read())["API_KEY"]
+
+pprint(get_bazaar_buy_order_value(get_bazaar_data()))
